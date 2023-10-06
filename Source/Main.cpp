4aa267d001e4256c04670310b5e548bd324e7863
Diego Moreira <chicodiegomoreira@gmail.com>
2023-10-06 00:01:53 -0300

    [✨Feature]: Implemented Insert function
    - Added a basic test at Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,21 @@
+#include "SlotMap.h"
+
 #include <iostream>
+#include <memory>
 
 int main()
 {
 	std::cout << "Simple Slot Map implementation." << std::endl;
+	
+	SlotMap<std::shared_ptr<int>> sm;
+	
+	auto id = sm.Insert(std::make_shared<int>());
+	id = sm.Insert(std::make_shared<int>());
+	id = sm.Insert(std::make_shared<int>());
+	id = sm.Insert(std::make_shared<int>());
+	id = sm.Insert(std::make_shared<int>());
+	
+	std::cout << "Last ID inserted into the Slot Map: "<< id << std::endl;
+
 	return 0;
 }
--- Source/SlotMap.h
+++ Source/SlotMap.h
@@ -8,15 +8,6 @@ class SlotMap
 {
     typedef unsigned long long ObjectId;
 
-public:
-
-    SlotMap() : _chunkSize{ N }, _objectTable{}, _freeIndices{}
-    {
-    }
-
-    ~SlotMap() = default;
-
-private:
     struct Object
     {
         ObjectId id;
@@ -26,4 +17,30 @@ private:
     const size_t _chunkSize;
     std::vector<std::array<Object, N>> _objectTable;
     std::vector<unsigned int> _freeIndices;
+
+public:
+
+    SlotMap() : _chunkSize{ N }, _objectTable{}, _freeIndices{}
+    {
+    }
+
+    ~SlotMap() = default;
+
+    ObjectId Insert(const T& object)
+    {
+        if (_freeIndices.empty())
+        {
+            _objectTable.push_back({});
+            
+            for (int i = _chunkSize - 1; i >= 0; --i) {
+                _freeIndices.push_back((_objectTable.size() - 1) * _chunkSize + i);
+            }
+        }
+
+        int free = _freeIndices.back();
+        _freeIndices.pop_back();
+
+        _objectTable[free / _chunkSize][free % _chunkSize].object = object;
+        return _objectTable[free / _chunkSize][free % _chunkSize].id = free;
+    }
 };
